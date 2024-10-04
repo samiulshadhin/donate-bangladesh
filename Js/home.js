ed43f93ccff85b5516a5144513f1f33e9f3ca75c
samiulshadhin <shadhin0844@gmail.com>
2024-10-04 21:55:43 +0600

    Done all function relation tasks

---
+++ Js/home.js
@@ -0,0 +1,56 @@
+// Clicking the "History" button will hide the donation data and display the history
+document.getElementById('history-button')
+    .addEventListener('click', function () {
+        buttonFillColor('history-button');
+        buttonStrokeColor('donation-button');
+        addHiddenClass('donation-page');
+        addHiddenClass('footer');
+        removeHiddenClass('history-page');
+    })
+
+
+// Clicking the "Donation" button will hide the history and display the donation data
+document.getElementById('donation-button')
+    .addEventListener('click', function () {
+        buttonFillColor('donation-button');
+        buttonStrokeColor('history-button');
+        addHiddenClass('history-page');
+        removeHiddenClass('donation-page');
+    })
+
+
+// Set addEventListener on Donate Now button
+document.getElementById('donate-now-button')
+    .addEventListener('click', function () {
+        const inputNumberValue = parseFloat(document.getElementById('card-input-field').value);
+
+
+        const accountBalance = document.getElementById('account-balance');
+        const accountBalanceNumber = parseFloat(accountBalance.innerText);
+
+        if(inputNumberValue > accountBalanceNumber || isNaN(inputNumberValue) || inputNumberValue <= 0 || inputNumberValue === ''){
+            alert('Invalid Donation Amount');
+            return;
+        }
+
+        accountBalance.innerText = accountBalanceNumber - inputNumberValue;
+
+        const cardBalance = document.getElementById('card-balance');
+        const cardBalanceNumber = parseFloat(cardBalance.innerText);
+
+        cardBalance.innerText = cardBalanceNumber + inputNumberValue;
+
+        const historyPage = document.getElementById('history-page');
+        const cardTitle = document.getElementById('card-title').innerText;
+        const notificationDiv = document.createElement('div');
+        notificationDiv.className = 'border-[1.5px] border-card-border-color rounded-2xl p-8';
+        notificationDiv.innerHTML = `
+            <h1 class="font-bold text-base lg:text-xl mb-2">${inputNumberValue} Taka is ${cardTitle}</h1>
+                <p class="text-sm lg:text-base">Date : ${new Date().toString()}</p>
+        `
+        historyPage.insertBefore(notificationDiv, historyPage.firstChild);
+        addHiddenClass('empty-history');
+
+        
+
+    })
---
+++ Js/library.js
@@ -0,0 +1,23 @@
+// Here are the common functions
+
+function buttonFillColor(id) {
+    const fillColor = document.getElementById(id);
+    fillColor.className = 'btn hover:bg-button-color bg-button-color px-6 text-lg font-semibold text-text-primary';
+    return fillColor;
+}
+
+function buttonStrokeColor(id) {
+    const strokeColor = document.getElementById(id);
+    strokeColor.className = 'btn bg-transparent border-[1.5px] border-button-border-color px-6 text-lg font-semibold text-text-secondary';
+    return strokeColor;
+}
+
+function removeHiddenClass(id) {
+    const removeHidden = document.getElementById(id);
+    return removeHidden.classList.remove('hidden');
+}
+
+function addHiddenClass(id) {
+    const addHidden = document.getElementById(id);
+    return addHidden.classList.add('hidden');
+}
